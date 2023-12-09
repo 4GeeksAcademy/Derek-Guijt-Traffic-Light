3a6b61d92cc7ae44cb91ac1258c21013d92bfb17
Derek Guijt <137216062+DerekGuijt@users.noreply.github.com>
2023-12-09 00:42:32 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,30 +2,33 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const TrafficLight = () => {
-  const [color, setColor] = useState("red");
+  const [color, setColor] = useState('red');
 
   const handleClick = (selectedColor) => {
     setColor(selectedColor);
   };
 
   return (
-    <div>
-      <div
-        className={`light red ${color === 'red' ? 'selected' : ''}`}
-        onClick={() => handleClick('red')}
-      ></div>
-      <div
-        className={`light yellow ${color === 'yellow' ? 'selected' : ''}`}
-        onClick={() => handleClick('yellow')}
-      ></div>
-      <div
-        className={`light green ${color === 'green' ? 'selected' : ''}`}
-        onClick={() => handleClick('green')}
-      ></div>
+    <div className="traffic-light">
+      <div className="pole"></div>
+      <div className="light-container">
+          <div
+            className={`light red ${color === 'red' ? 'selected' : ''}`}
+            onClick={() => handleClick('red')}
+          ></div>
+          <div
+            className={`light yellow ${color === 'yellow' ? 'selected' : ''}`}
+            onClick={() => handleClick('yellow')}
+          ></div>
+          <div
+            className={`light green ${color === 'green' ? 'selected' : ''}`}
+            onClick={() => handleClick('green')}
+          ></div>
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
+ReactDOM.render(<TrafficLight />, document.getElementById('app'));
 
-export default App;
+export default TrafficLight;
